c01eca35e724e8626e500408074376bff46bead2
thalescr7 <128698680+thalescr7@users.noreply.github.com>
2023-10-03 15:49:48 -0300

    aula16

--- aula13/aula13.js/aula13.js
+++ aula13/aula13.js/aula13.js
@@ -1,5 +1,6 @@
 var valor;
 var operacao;
+var mermoria=0;
 
 function efetuar_soma() {
     if (typeof valor != typeof undefined && operacao != typeof undefined) {
@@ -13,9 +14,44 @@ function efetuar_soma() {
     operacao = soma;
 }
 
+function efetuar_subtracao() {
+    if (typeof valor != typeof undefined && operacao != typeof undefined) {
+        valor = operacao(valor, Number(document.getElementById("valor").value));
+    } else {
+        valor = Number(document.getElementById("valor").value);
+        document.getElementById("valor").value = "";
+    }
+
+
+    operacao = subtracao;
+}
+
+function efetuar_divisao() {
+    if (typeof valor != typeof undefined && operacao != typeof undefined) {
+        valor = operacao(valor, Number(document.getElementById("valor").value));
+    } else {
+        valor = Number(document.getElementById("valor").value);
+        document.getElementById("valor").value = "";
+    }
+
+
+    operacao = divisao;
+}
+
+function efetuar_multiplicacao() {
+    if (typeof valor != typeof undefined && operacao != typeof undefined) {
+        valor = operacao(valor, Number(document.getElementById("valor").value));
+    } else {
+        valor = Number(document.getElementById("valor").value);
+        document.getElementById("valor").value = "";
+    }
+
+
+    operacao = multiplicacao;
+}
 
 function resultado() {
-    document.getElementById("valor").value = 
+    document.getElementById("valor").value =
         operacao(valor, Number(document.getElementById("valor").value));
 
     valor = undefined
@@ -62,9 +98,52 @@ function insere_9() {
     document.getElementById("valor").value = document.getElementById("valor").value.concat(9);
 }
 
+function porcentagem(){
+    if(typeof valor == typeof undefined){
+     document.getElementById("valor").value = 
+     Number(document.getElementById("valor").value)/100;
+    }else {
+        document.getElementById("valor").value = 
+        (valor / 100) *
+        Number(document.getElementById("valor").value);
+    }
+}
+
+function adicionar_ponto(){
+    if(!document.getElementById("valor").value.includes(".")){
+     document.getElementById("valor").value = document.getElementById("valor").value.concat(".");
+    }
+ }
 
 function apagar() {
-    document.getElementById("valor").value = document.getElementById("valor").value.substring(0, document.getElementById("valor").value.legth - 1)
+    document.getElementById("valor").value = "";
+    valor = undefined;
+    operacao = undefined;
+}
+
+function apagar_x() {
+    document.getElementById("valor").value = document.getElementById("valor").value.substring
+        (0, document.getElementById("valor").value.length
+            - 1)
+
+
+}
+
+function add_num() {
+    valor = mais_num(Number(document.getElementById("valor").value))
+}
+
+function subt_num() {
+    valor = men_num(Number(document.getElementById("valor").value))
+}
+
+function apag_memoria(){
+    memoria = 0
+    return console.log(memoria)
+}
+function most_memo(){
+    document.getElementById("valor").value = memoria
+    
 }
 
 function soma(n1, n2) {
@@ -100,3 +179,19 @@ function dividir() {
         Number(document.getElementById("n2").value))
 }
 
+
+function mais_num(n1){
+    memoria += n1
+    return console.log(memoria)
+}
+function men_num(n1){
+    memoria -= n1
+    return console.log(memoria)
+}
+
+
+let n1 = document.getElementById("n1")
+let n2 = document.getElementById("n2")
+function somar() {
+    document.getElementById("resultado").value = soma(Number(n1.value), Number(n2.value))
+}
